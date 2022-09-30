7917fcc7ed38f974e6496e17a97448991346e7fd
Charlemagne44 <collinhay44@gmail.com>
2022-09-30 16:16:24 -0400

    object structure

--- model/object.go
+++ model/object.go
@@ -1,6 +1,22 @@
 package model
 
 type Object struct {
-	Parent *Object
-	Name   string
+	Name     string
+	Capacity int
+}
+
+func (o *Object) GetName() string {
+	return o.Name
+}
+
+func (o *Object) GetCapacity() int {
+	return o.Capacity
+}
+
+func (o *Object) SetName(name string) {
+	o.Name = name
+}
+
+func (o *Object) SetCapacity(capacity int) {
+	o.Capacity = capacity
 }
